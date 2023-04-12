a64177293b5818f9182423e8dca49e78d361b2ca
Ryan Moloy <19rem3@queensu.ca>
2023-04-12 04:05:32 -0400

    updates preprocessing file, normalization and filtering completed

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 
 filename = sys.argv[1]
@@ -25,30 +26,44 @@ raw_dataset_times = raw_dataset.iloc[:, 0]
 
 # reduce noise using moving average filter
 window_size = 31
-norm_dataset_data = raw_dataset_data.rolling(window_size).mean()
+filt_dataset_data = raw_dataset_data.rolling(window_size).mean()
 
 # adjust the rows of the dataset (both data and labels) to drop (window_size - 1) rows at beginning
 # must do this since rolling av filter removes these rows
-norm_dataset_data = norm_dataset_data.truncate(before=(window_size-1))
+filt_dataset_data = filt_dataset_data.truncate(before=(window_size-1))
 raw_dataset_labels = raw_dataset_labels.truncate(before=(window_size-1))
 raw_dataset_times = raw_dataset_times.truncate(before=(window_size-1))
 
 
 ### export data at current step for feature extraction (Step 5) ###
-pieces = [raw_dataset_times, norm_dataset_data, raw_dataset_labels]
-norm_dataset = pd.concat(pieces, axis=1)
+pieces = [raw_dataset_times, filt_dataset_data, raw_dataset_labels]
+filt_dataset = pd.concat(pieces, axis=1)
 
-new_path = 'feature-extraction-ready\\normalized-' + filename.rsplit('\\', 1)[-1]
+new_path = 'feature-extraction-ready\\filtered-' + filename.rsplit('\\', 1)[-1]
+filt_dataset.to_csv(new_path, index=False)
+
+print('Data filtered and exported to feature-extraction-ready folder.')
+
+
+### detect and remove outliers ###
+
+
+
+### normalize the data ###
+sc = preprocessing.StandardScaler()
+
+norm_dataset_data = sc.fit_transform(filt_dataset_data)
+# initialize a busted index
+
+norm_dataset_data = pd.DataFrame(data=norm_dataset_data, columns=['Acceleration x (m/s^2)','Acceleration y (m/s^2)','Acceleration z (m/s^2)','Absolute acceleration (m/s^2)'])
+raw_dataset_times.index = range(len(raw_dataset_times))
+raw_dataset_labels.index = range(len(raw_dataset_labels))
+
+pieces = [raw_dataset_times, norm_dataset_data, raw_dataset_labels]
+norm_dataset = pd.concat(pieces, axis=1, join="inner")
+
+
+new_path = 'shuffling-splitting-ready\\normalized-' + filename.rsplit('\\', 1)[-1]
 norm_dataset.to_csv(new_path, index=False)
 
-print('Data normalized and exported to feature-extraction-ready folder.')
-###
-
-# detect and remove outliers
-
-
-
-# normalize
-
-
-
+print('Data normalized and exported to shuffling-splitting-ready folder.')
